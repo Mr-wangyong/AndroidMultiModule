5356c3a00481c39b0093936cae6a0dfdc9fda926
cheng.wangyong <cheng.wangyong@immomo.com>
2019-05-20 20:48:12 +0800

    frist commit

---
+++ module_girl/src/main/java/com/mrwang/module_girl/GirlActivity.kt
@@ -0,0 +1,12 @@
+package com.mrwang.module_girl
+
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+
+class GirlActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
